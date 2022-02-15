56cb9860c2b7376967f7d16411eaecd8f2465798
chaelin1211 <cofls749@naver.com>
2022-02-15 17:10:30 +0900

    cytoscape layout 적용 오류 수정 / element 연동 데이터 가져오는 부분 수정

--- packages/main/app1/src/components/graph.js
+++ packages/main/app1/src/components/graph.js
@@ -1,29 +1,34 @@
 import React, { Component, createRef } from "react";
 import CytoscapeComponent from "../utils/cytoscape";
 import CytoscapeStyleSheet from "../assets/cytoscapeStyleSheet"
-
-const api = require('../api/neo4jApi');
+const memberApi = require('../api/memberApi');
 
 class Graph extends Component {
   constructor() {
     super();
     this.ref = createRef();
-    this.layout = { name: "cose-bilkent" };
-    this.state = {};
+    this.state = { elements: [], layout: { name: 'grid', row: 1 } };
+  }
+
+  componentDidMount() {
+    memberApi.getMemberGraph().then((v) => {
+      this.setState({ elements: v });
+    });
   }
 
   render() {
     return (
       <div>
         <CytoscapeComponent
-        elements={api.getGraph()}
-        stylesheet={CytoscapeStyleSheet()}
-        layout={this.layout}
-        style={{ width: "100%", height: "400px" }}
-        cy={cy => (this.ref = cy)}
-      />
+          elements={this.state.elements}
+          style={{ width: "100%", height: "400px" }}
+          stylesheet={CytoscapeStyleSheet()}
+          cy={(cy) => {
+            this.cy = cy
+            cy.layout(this.state.layout).run()
+          }}
+        />
       </div>
-      
     );
   }
 }
